502abfa8f1444de88ff4e0e8e0b7814a1f271cb1
Placinta <placinta@176134.no-reply.drupal.org>
2013-01-30 00:02:14 +0200

    Add link to notification list.

--- EventTicket/urls.py
+++ EventTicket/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
     # Serve static content.
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
     (r'^register/', 'ticket.views.register_view'),
+
     url(r'^email-notification/add/', 'ticket.views.email_notification_add', name='AddEmailNotification'),
-    (r'^email-notification/', 'ticket.views.email_notification_list'),
+    url(r'^email-notification/', 'ticket.views.email_notification_list', name="ListEmailNotifications"),
 )
